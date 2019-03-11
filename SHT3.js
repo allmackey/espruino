c4180f32a7cba16bbdecf4af9a959494ce7c924a
allmackey <Allen.Mackey@gmail.com>
2019-03-11 14:53:49 -0400

    Update SHT3.js

--- SHT3.js
+++ SHT3.js
@@ -30,11 +30,12 @@ SHT2x.prototype.checkCrc = function(bytes, bytesLen, checksum) {
 };
 
 SHT2x.prototype.readTemperature = function() {
-  print("v0");
+  print("v1");
+  print(this.addr);
   this.i2c.writeTo(this.addr, 0xe3);
   var result = this.i2c.readFrom(this.addr, 3);
   var value = (result[0] << 8) | (result[1] & ~0x03);
-  this.checkCrc(result, 2, result[2]);
+  //this.checkCrc(result, 2, result[2]);
   if (!value) {
     return null;
   }
@@ -45,7 +46,7 @@ SHT2x.prototype.readHumidity = function() {
   this.i2c.writeTo(this.addr, 0xe5);
   var result = this.i2c.readFrom(this.addr, 3);
   var value = (result[0] << 8) | (result[1] & ~0x03);
-  this.checkCrc(result, 2, result[2]);
+  //this.checkCrc(result, 2, result[2]);
   if (!value) {
     return null;
   }

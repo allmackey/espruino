4f0bf4606bc02f53cc1da2ccec6df959f73d653f
allmackey <Allen.Mackey@gmail.com>
2019-03-11 21:55:03 -0400

    Update SHT3.js

--- SHT3.js
+++ SHT3.js
@@ -3,7 +3,7 @@
 /* Module for SHT20/21/25 Humidity and Temperature Sensor IC */
 
 var C = {
-  ADDR: 0x40,
+  ADDR: 0x44,
   POLYNOMIAL: 0x131
 };
 
@@ -22,7 +22,7 @@ SHT2x.prototype.checkCrc = function(bytes, bytesLen, checksum) {
     crc ^= bytes[i];
     for (var bit = 8; bit > 0; --bit) {
       crc = (crc & 0x80) ? ((crc << 1) ^ C.POLYNOMIAL) : (crc << 1);
-    } 
+    }
   }
   if (crc !== checksum) {
     throw "CRC Error";
@@ -30,18 +30,18 @@ SHT2x.prototype.checkCrc = function(bytes, bytesLen, checksum) {
 };
 
 SHT2x.prototype.readTemperature = function() {
-  print("v7");
+  print("v0");
   print(this.addr);
-  this.i2c.writeTo(this.addr, 0xe3);
-  var t=getTime()+15;
+  var t=getTime()+1000;
   while(getTime()<t);
+  this.i2c.writeTo(this.addr, 0xe3);
   var result = this.i2c.readFrom(this.addr, 3);
   var value = (result[0] << 8) | (result[1] & ~0x03);
   //this.checkCrc(result, 2, result[2]);
-  if (!value) { 
+  if (!value) {
     return null;
   }
-  return -49 + 315 / 65535.0 * value;
+  return -49.0 + 315.0 / 65535.0 * value;
 };
 
 SHT2x.prototype.readHumidity = function() {
@@ -52,5 +52,5 @@ SHT2x.prototype.readHumidity = function() {
   if (!value) {
     return null;
   }
-  return  100 / 65536.0 * value;
+  return  100.0 / 65535.0 * value;
 };

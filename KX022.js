c655ab34105ec12f37b6acd54910ea891b20fdcd
stephmack <mackey.stephen@gmail.com>
2019-07-01 15:05:45 -0400

    Update KX022.js

--- KX022.js
+++ KX022.js
@@ -36,7 +36,7 @@ function LIS2MDL(options,r,w) {
   //this.w(REG.CFG_B, 0x01);
   // data ready irq, block data read
   //this.w(REG.CFG_C, 0x11);
-}
+};
 
 //tt
 LIS2MDL.prototype.init = function() {
@@ -60,6 +60,13 @@ LIS2MDL.prototype.read = function() {
     yH: d.getInt8(3,1),
     zH: d.getInt8(5,1)
   };
+  
+  LIS2MDL.prototype.readcntr = function() {
+  var d = new DataView(this.r(REG.CNTL1,1).buffer);
+  return {
+    res:  d.getInt8(0,1)
+  };
+  
 };
 exports.LIS2MDL = LIS2MDL;
 

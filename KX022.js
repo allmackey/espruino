9b1c90e510d73401eb1b02b41b2745274515430e
allmackey <Allen.Mackey@gmail.com>
2019-07-16 08:54:33 -0400

    Update KX022.js

--- KX022.js
+++ KX022.js
@@ -65,7 +65,7 @@ LIS2MDL.prototype.init = function() {
 //tt
 LIS2MDL.prototype.read = function() {
   var d = new DataView(this.r(REG.OUTX_L,6).buffer);
-  print(
+  print("v1");
   return {
     x:  d.getInt16(0,2),
     y:  d.getInt16(2,2),

c9fea0adf04fb69090c78b0d0e5154b529bf9e4c
allmackey <Allen.Mackey@gmail.com>
2019-08-05 14:34:42 -0400

    Update NRFScripts_New.js

--- NRFScripts_New.js
+++ NRFScripts_New.js
@@ -1,4 +1,3 @@
-//var b = digitalRead(D25);
 var i2c = new I2C();
 i2c.setup({ scl : D30, sda: D31 });
 var acc = require("https://github.com/allmackey/espruino/blob/master/KX022.js").connectI2C(i2c);
@@ -28,6 +27,7 @@ var yHOld = 0;
 var zLOld = 0;
 var zHOld = 0;
 var cnt = 0;
+var tempd = 0;
 var batt = Math.round(NRF.getBattery()*1000);
 var i = 0;
 var sht =  require("https://github.com/allmackey/espruino/blob/master/SHT3.js").connect(i2c,0x44);
@@ -36,8 +36,8 @@ var tL = 0;
 var hH = 0;
 var hL = 0;
 
-var eq1l = -1.08695653;
-var eq2l= 60.52173913;
+var eq1l = -1.086956522;
+var eq2l= 66.52173913;
 var eq1 = parseFloat(require("Storage").read("eq1"));
 var eq2 = parseFloat(require("Storage").read("eq2"));
 var f = 0;
@@ -107,37 +107,37 @@ var t = setInterval(function () {
     digitalWrite(D26, 1);
     reset(true);
   }*/
-  xL = acc.read().xL;
-  xH = acc.read().xH;
-  yL = acc.read().yL;
-  yH = acc.read().yH;
-  zL = acc.read().zL;
-  zH = acc.read().zH;
-  xP = acc.read().xP;
-  yP = acc.read().yP;
-  zP = acc.read().zP;
-  Tilt = acc.read().tilt;
-  plato = eq1*Tilt+eq2;
-  SG = 1 + (plato / (258.60 - ( (plato/258.20) *227.10) ) );
+  tempd = acc.read();
+  xL = tempd.xL;
+  xH = tempd.xH;
+  yL = tempd.yL;
+  yH = tempd.yH;
+  zL = tempd.zL;
+  zH = tempd.zH;
+  xP = tempd.xP;
+  yP = tempd.yP;
+  zP = tempd.zP;
+  Tilt = tempd.tilt;
+  plato = (eq1*Tilt)+eq2;
+  print(Tilt);
+  print(plato);
+  SG = 1 + (plato / (258.600 - ( (plato/258.200) *227.100) ) );
   if (SG >= 1) {
     SGH = 1;
   } else {
      SGH = 0;
   }
-  SGL = parseInt((SG.toString()).substring(2, 5));
-  SGLL =SGL & 0xff;
-  SGLH= (SGL >> 8);
   print(SG);
-  print(SGH);
-  print(SGL);
-  print(SGLH);
-  print(SGLL);
+  SG = Math.round(SG*10000);
+  //SGL = parseInt((SG.toString()).substring(2, 5));
+  SGL =SG & 0xff;
+  SGH= (SG >> 8);
   tH = sht.readData().tH;
   tL = sht.readData().tL;
   hH = sht.readData().hH;
   hL = sht.readData().hL;
   require("https://github.com/allmackey/espruino/blob/master/BrewBeacon.js").advertise({
-  uuid : [SGH, SGLH, SGLL, zP, yP, xP, tH, tL, hH, hL, xL, xH, yL, yH, zL, zH], // ibeacon uuid
+  uuid : [0, SGH, SGL, zP, yP, xP, tH, tL, hH, hL, xL, xH, yL, yH, zL, zH], // ibeacon uuid
   major : batt, // optional
   minor : 0x0001, // optional
   rssi : -59, // optional RSSI at 1 meter distance in dBm
@@ -150,7 +150,7 @@ var t = setInterval(function () {
     }
     cnt++;
   } else {
-    cnt = 0; 
+    cnt = 0;
   }
   xLOld = xL;
   xHOld = xH;
